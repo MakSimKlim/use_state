8907ad9deca869f085bec9fe0802d1d8f962d47b
MakSimKlim <83maxomus@mail.ru>
2024-12-21 19:49:04 +0500

    All atributes ON

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 //import logo from './logo.svg';
 import './App.css';
-//import Counter from './Counter';
-//import Persone from './Persone';
-//import Item from './Item';
-//import Modal from './Modal';
+import Counter from './Counter';
+import Persone from './Persone';
+import Item from './Item';
+import Modal from './Modal';
 import Tasklist from './tasklist/Tasklist';
 
 function App() {
   return (
     <div className="App">
-      {/* <Counter/>  <hr/>
+      <Counter/>  <hr/>
       <Persone/>  <hr/>
       <Item />    <hr/>
-      <Modal/>    <hr/>       */}
+      <Modal/>    <hr/>      
       <Tasklist />
     </div>
   );
